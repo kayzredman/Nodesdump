04e5b697d62094af51c60a42da0b58a6e868cae8
Kwekku <rowland.kay.jones@gmail.com>
2020-05-12 12:01:53 +0000

    New Update

--- index.js
+++ index.js
@@ -32,8 +32,28 @@ const url = require('url');
 // *********  SERVER **************
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
+const data = fs.readFileSync('./1-node-farm/starter/dev-data/data.json', 'utf-8');
+const dataObj = JSON.parse(data);
+
+
 const server = http.createServer((req, res) => {
-   res.end('Hello from the server!!!'); 
+   const pathName = req.url;
+   
+   if (pathName === '/' || pathName === '/overview') {
+       res.end('This is the Overview Page!');
+   }else if (pathName === '/products'){
+       res.end('You have reached the Products Page!');
+   }else if (pathName === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json'});
+    res.end(data);
+    } else {
+       res.writeHead(404, {
+           'Content-type': 'text/html',
+           'NodesSysMsg': 'hello-nodejs'
+       });
+       res.end('<h1>Blimey!..Page not found</h1>');
+   }
+      
 });
 
 server.listen(8000, '127.0.0.1', () => {
